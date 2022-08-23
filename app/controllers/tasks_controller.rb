0964f62e97a5c88ccc4dcd5d766074329986d29b
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-23 14:22:47 +0100

    added create task option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,18 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
